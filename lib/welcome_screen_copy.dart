66a5915ed4181dd7c41b1604e14b5e70a30d0994
molokows <d@molokov.de>
2021-07-23 21:01:19 +0300

    Removed listview generation from welcome screen.

---
+++ lib/welcome_screen_copy.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:newnavapp/iconContent_class.dart';
+import 'package:newnavapp/reusablecard.dart';
+
+enum Gender { male, female, aikidoka }
+
+final activeColour = Colors.purple;
+final inactiveColour = Colors.blue;
+final aikinactiveColour = Colors.white;
+final aikiactiveColour = Colors.black26;
+final backroundColour = Colors.black54;
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({Key? key, required this.title}) : super(key: key);
+  static const String id = 'welcome_screen';
+  final String title;
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  int _counter = 100;
+  Color maleCardColour = inactiveColour;
+  Color femaleCardColour = inactiveColour;
+  Color aikidokaCardColour = aikinactiveColour;
+
+  void updateColour(gender) {
+    gender == Gender.male
+        ? maleCardColour = activeColour
+        : femaleCardColour = activeColour;
+    gender == Gender.male
+        ? femaleCardColour = inactiveColour
+        : maleCardColour = inactiveColour;
+    gender == Gender.aikidoka
+        ? aikidokaCardColour = aikinactiveColour
+        : aikidokaCardColour = aikiactiveColour;
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(WelcomeScreen.id),
+        backgroundColor: Colors.teal[800],
+      ),
+      body: ListView.builder(
+          itemCount: 7,
+          itemBuilder: (BuildContext context, int index) {
+            return ReusableCard(
+              onPress: () {
+                setState(() {
+                  updateColour(Gender.female);
+                });
+              },
+              ad: 'empty',
+              colour: femaleCardColour,
+              cardChild:
+                  IconContent(icon: FontAwesomeIcons.venus, label: "female"),
+            );
+            // This remains unchanged.
+          }),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
